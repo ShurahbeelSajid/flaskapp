253dbeb1d6d84e78f9548b92606c63cc089d2010
Shuraheel Sajid <shurahbeelsajid008@gmail.com>
2024-05-21 17:58:20 +0500

    another new route

--- app.py
+++ app.py
@@ -13,7 +13,10 @@ def hello_world1():
 def hello_world1():
     return 'Hello, another route!'
 
-
+@app.route('/1')
+def hello_world1():
+    return 'Hello, another route!'
+    
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=2100)
 
